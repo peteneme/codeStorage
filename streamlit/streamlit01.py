58f95cfca07706d00cfecbf7b9f560694a8b7396
Peter Nemec <peteneme@centrum.sk>
2021-05-08 10:13:53 +0200

    ex01

---
+++ streamlit/streamlit01.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+import numpy
+
+st.write("""
+# H1
+## H2
+### H3
+*Italic text*
+
+
+https://docs.streamlit.io/en/stable/api.html
+""")
+
+
+### random table
+df1 = pd.DataFrame( numpy.random.randn(50, 20), 
+                    columns=('col %d' % i for i in range(20)))
+
+my_table = st.table(df1)
+
+
+### read data from yf
+tickerData = yf.Ticker("AAPL")
+tickerDf = tickerData.history(period='1d', start='2010-1-1', stop='2020-1-1')
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
