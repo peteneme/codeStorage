28dfc7ee5916f8f7108112d96480eb0a1f4b14a4
Peter Nemec <peteneme@centrum.sk>
2021-05-18 18:21:50 +0200

    simple plots

---
+++ matplotlib/matplotlib01.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+
+### https://matplotlib.org/stable/tutorials/introductory/pyplot.html
+
+
+import numpy as np
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+
+
+
+
+### line , x axis default
+plt.plot([1, 2, 3, 4])
+
+### red dot
+plt.plot([1, 2, 3, 4], [0, 3, 7, 4], 'ro')
+
+
+
+plt.xlabel('x axis could be default data', fontsize=14, color='red')
+plt.ylabel('some numbers')
+
+plt.axis([0, 6, 0, 20])
+
+plt.title('title')
+plt.grid(True)
+
+plt.show()
