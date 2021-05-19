4e7901a7f2a6d58aea3b6cf842e4276ac3bff2bc
Peter Nemec <peteneme@centrum.sk>
2021-05-19 14:25:41 +0200

    link

--- pandas/pandas01.py
+++ pandas/pandas01.py
@@ -3,6 +3,8 @@
 import numpy as np
 import pandas as pd
 
+print("https://pandas.pydata.org/pandas-docs/stable/user_guide/10min.html")
+
 ### Series
 s = pd.Series([1, 3, 5, np.nan, 6, 8])
 print(s)
