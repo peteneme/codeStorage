656604040059a0196db3936000e6f72cbd5a0a6c
Peter Nemec <peteneme@centrum.sk>
2021-06-16 09:48:04 -0700

    chunked cli switch

--- mix/cwebserver/cwebserver.c
+++ mix/cwebserver/cwebserver.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define DEFAULT_PORT "80"
 #define STRING_LEN    10
@@ -12,6 +13,8 @@ int main(int argc,char* argv[])
     char used_port_string[STRING_LEN] = DEFAULT_PORT;
     char arg_value[STRING_LEN];
     int used_port = atoi(DEFAULT_PORT);
+    bool chunked_mode=false;
+
 
     //printf("Program Name Is: %s\n", argv[0]);
     if(argc>1)
@@ -33,12 +36,14 @@ int main(int argc,char* argv[])
                 }    
             };
 
+            if (strcmp(arg_value, "--chunked")==0) { chunked_mode = true; };
+            
             
         }
         
     }
 
-    printf("USED_PORT: %d\n", used_port);
+    printf("BINARY: %s, USED_PORT: %d, CHUNKED_MODE: %s\n", argv[0], used_port, chunked_mode ? "true" : "false");
 
 
     // PRINT ERROR IF OCCURS
