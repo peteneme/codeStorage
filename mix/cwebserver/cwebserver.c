40b2f71278f91d71540ae930438581c5df51cda9
Peter Nemec <peteneme@centrum.sk>
2021-06-16 23:21:57 -0700

    more cli param

--- mix/cwebserver/cwebserver.c
+++ mix/cwebserver/cwebserver.c
@@ -4,49 +4,62 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define DEFAULT_PORT "80"
+
+// DEFINITIONS ----------------------------------------------------------------
+#define DEFAULT_PORT  80
 #define STRING_LEN    10
 
-int main(int argc,char* argv[])
+// FUNCTION DECLARATIONS ------------------------------------------------------
+void help_text(void);
+
+
+
+// FUNCTION DEFINITIONS -------------------------------------------------------
+void help_text(void) {
+    printf("Input CLI parameters:\n");
+    printf("--help               - Print help\n");
+    printf("--port NUMBER        - Webserver PORT NUMBER\n");
+    printf("--keep-alive SECONDS - Switch on keep-alive by inserting NUMBER of seconds\n");
+    printf("--root_dir DIRECTORY - Set webserver root directory, by default is ./ \n");
+    printf("--chunked            - Switch on http1.1 chunked mode, by default is off\n");
+}
+
+
+// MAIN ***********************************************************************
+int main(int argc, char* argv[])
 {
-    int counter;
-    char used_port_string[STRING_LEN] = DEFAULT_PORT;
-    char arg_value[STRING_LEN];
-    int used_port = atoi(DEFAULT_PORT);
-    bool chunked_mode=false;
+    int used_port     = DEFAULT_PORT;
+    int keep_alive    = 0;
+    bool chunked_mode = false;
+    char root_dir[256] = "./";
 
 
-    //printf("Program Name Is: %s\n", argv[0]);
-    if(argc>1)
-    {
-        for(counter=0; counter<argc; counter++)
-        {
-            sprintf(arg_value, "%s", argv[counter]);
-
-            if (strcmp(arg_value, "--port")==0) 
-            {
-                counter++;
-                char tmp_port[STRING_LEN];
-                sprintf(tmp_port, "%s", argv[counter]);
-                // JUST IGNORE NON-INTERGER VALUES
-                if ((strcmp(tmp_port, "(null)")!=0) && (atoi(tmp_port)>0) && (atoi(tmp_port)<=0xffff))
-                { 
-                    sprintf(used_port_string, "%s", argv[counter]);
-                    used_port = atoi(used_port_string);
-                }    
-            };
-
-            if (strcmp(arg_value, "--chunked")==0) { chunked_mode = true; };
-            
-            
+    // CLI PARAMETERS PARSING
+    if(argc>1) {
+        int counter;
+        for(counter=0; counter<argc; counter++) {
+            if (strcmp(argv[counter], "--help")==0) { help_text(); return errno; };
+            if (strcmp(argv[counter], "--port")==0) 
+                // JUST IGNORE NON-INTERGER VALUES OR NEXT OR NON-EXISTING PARAMETERS
+                if ((strcmp(argv[counter+1], "(null)")!=0) && (atoi(argv[counter+1])>0) && (atoi(argv[counter+1])<=0xffff))                
+                    used_port = atoi(argv[counter+1]);
+            if (strcmp(argv[counter], "--keep-alive")==0) 
+                // JUST IGNORE NON-INTERGER VALUES OR NEXT OR NON-EXISTING PARAMETERS
+                if ((strcmp(argv[counter+1], "(null)")!=0) && (atoi(argv[counter+1])>0))                
+                    keep_alive = atoi(argv[counter+1]);
+            if (strcmp(argv[counter], "--root_dir")==0) 
+                // JUST IGNORE NON-EXISTING PARAMETERS
+                if ((strcmp(argv[counter+1], "(null)")!=0)) sprintf(root_dir, "%s", argv[counter+1]);                                                      
+            if (strcmp(argv[counter], "--chunked")==0) chunked_mode = true;  
         }
-        
     }
 
-    printf("BINARY: %s, USED_PORT: %d, CHUNKED_MODE: %s\n", argv[0], used_port, chunked_mode ? "true" : "false");
 
+    // PRINT ACCEPTED PARAMETERS
+    printf("BINARY: %s, USED_PORT: %d, CHUNKED_MODE: %s, ", argv[0], used_port, chunked_mode ? "true" : "false");
+    printf("KEEP-ALIVE: %d, ROOT_DIR: %s\n",  keep_alive, root_dir);
 
     // PRINT ERROR IF OCCURS
     if (errno>0) perror("ERR:");
-    return 0;
+    return errno;
 }
