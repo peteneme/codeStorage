dd15476887e01b4246d353f538509c3d1d0500ae
Peter Nemec <peteneme@centrum.sk>
2021-06-23 20:44:05 +0200

    example

---
+++ mix/lambda/lambda01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+
+add_one = lambda x: x + 1
+print(add_one(2))
+
+full_name = lambda first, last: f'Full name: {first.title()} {last.title()}'
+print(full_name('guido', 'van rossum'))
+
+sum = lambda x, y: x + y
+print(sum(1,6))
